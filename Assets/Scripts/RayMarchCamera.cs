dff95718064f665ccea95d32ef58f6cd0d9904e1
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2019-10-01 23:06:46 -0600

    Initial commit

---
+++ Assets/Scripts/RayMarchCamera.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RayMarchCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _shader;
+
+    public Material _raymarchMaterial
+    {
+        get 
+        {
+            if (!_raymarchMat && _shader)
+            {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _raymarchMat;
+        }
+    }
+
+    private Material _raymarchMat;
+
+    public Camera _camera
+    {
+        get
+        {
+            if (!_cam)
+            {
+                _cam = GetComponent<Camera>();
+
+            }
+
+            return _cam;
+        }
+    }
+
+    private Camera _cam;
+
+    public float _maxDistance;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!_raymarchMaterial)
+        {
+            Graphics.Blit(source, destination);
+        }
+
+        _raymarchMaterial.SetMatrix("_CamFrustrum", CamFrustrum(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+        _raymarchMaterial.SetFloat("_maxDistance", _maxDistance);
+
+        RenderTexture.active = destination;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _raymarchMaterial.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+        //BL
+        GL.MultiTexCoord2(0, 0, 0);
+        GL.Vertex3(0, 0, 3);
+        //BR
+        GL.MultiTexCoord2(0, 1, 0);
+        GL.Vertex3(1, 0, 2);
+        //TR
+        GL.MultiTexCoord2(0, 1, 1);
+        GL.Vertex3(1, 1, 1);
+        //TL
+        GL.MultiTexCoord2(0, 0, 1);
+        GL.Vertex3(0, 1, 0);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    private Matrix4x4 CamFrustrum(Camera cam)
+    {
+        Matrix4x4 frustrum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView / 2) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 tl = -Vector3.forward - goRight + goUp;
+        Vector3 tr = -Vector3.forward + goRight + goUp;
+        Vector3 bl = -Vector3.forward - goRight - goUp;
+        Vector3 br = -Vector3.forward + goRight - goUp;
+
+        frustrum.SetRow(0, tl);
+        frustrum.SetRow(1, tr);
+        frustrum.SetRow(2, br);
+        frustrum.SetRow(3, bl);
+
+        return frustrum;
+    }
+}
---
+++ Assets/Shaders/VolumeShader.shader
@@ -0,0 +1,104 @@
+﻿Shader "AbstractMath/VolumeShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			uniform float4x4 _CamFrustrum, _CamToWorld;
+			uniform float _maxDistance;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+				float3 ray : TEXCOORD1;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+				half index = v.vertex.z;
+				v.vertex.z = 0;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+
+				o.ray = _CamFrustrum[(int)index].xyz;
+				o.ray /= abs(o.ray.z);
+				o.ray = mul(_CamToWorld, o.ray);
+                return o;
+            }
+
+			float sdSphere(float3 p, float s) 
+			{
+				return length(p) - s;
+			}
+
+			float distanceField(float3 p) 
+			{
+				float sphere1 = sdSphere(p - float3(0, 0, 0), 2);
+				return sphere1;
+			}
+
+			fixed4 raymarching(float3 ro, float3 rd) 
+			{
+				fixed4 result = fixed4(1, 1, 1, 1);
+				const int max_iteration = 64;
+				float t = 0;//Distance travelled.
+
+				for (int i = 0; i < max_iteration; i++) 
+				{
+					if (t > _maxDistance) 
+					{
+						//Environment
+						result = fixed4(rd, 1);
+						break;
+					}
+
+					float3 p = ro + rd * t;
+					//check for a hit in distance field
+					float d = distanceField(p);
+					if (d < 0.01) 
+					{
+						result = fixed4(1, 1, 1, 1);
+						break;
+					}
+					t += d;
+				}
+
+				return result;
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float3 rayDirection = normalize(i.ray.xyz);
+				float3 rayOrigin = _WorldSpaceCameraPos;
+
+				fixed4 result = raymarching(rayOrigin, rayDirection);
+
+				return result;
+            }
+            ENDCG
+        }
+    }
+}
